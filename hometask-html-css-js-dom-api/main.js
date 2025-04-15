c7efcfe10bd3a681dc923fa10eb012a70069768a
Yalale Lahunter97 <yahorlahunblr@icloud.com>
2025-04-15 20:36:17 +0300

    Reduce index

--- hometask-html-css-js-dom-api/main.js
+++ hometask-html-css-js-dom-api/main.js
@@ -7,24 +7,40 @@ const HeaderData = {
 
 const CardData = [
     {
+        cardstyle: "grey",
+        headerstyle: "blue",
+        textstyle: "blue",
+        buttonstyle: "white",
         title: "Startup Framework",
         text: "Startup is a powerful tool for quick and convenient proto-typing of your projects. It will fit most projects because it contains up-to-date and modern web elements.",
     },
 
     {  
+        cardstyle: "white",
+        headerstyle: "blue",
+        textstyle: "grey",
+        buttonstyle: "green",
         title: "Web Generator",
         text: "Startup is a powerful tool for quick and convenient proto-typing of your projects. It will fit most projects because it contains up-to-date and modern web elements.",
     },
 
     {
+        cardstyle: "blue",
+        headerstyle: "white",
+        textstyle: "white",
+        buttonstyle: "white",
         title: "Slides 4",
         text: "All of these components are made in the same style, and can easily be inegrated into projects, allowing you to create hundreds of solutions for your future projects.",
     },
 
     {
+        cardstyle: "image",
+        headerstyle: "white",
+        textstyle: "white",
+        buttonstyle: "white",
         title: "Postcards",
         text: "All frequently used elements are now in symbols. Use them to create interfaces really fast. Easily change icons, colors and text. Add new symbols to customize your design.",
-    },
+    },    
 ];
 
 // For header
@@ -49,7 +65,7 @@ Header.prepend(HeaderElement);
 const CardValue = document.createDocumentFragment();
 const CardSection = document.createElement("section");
 
-CardData.forEach((item, index) => {
+CardData.forEach((item) => {
     
     const CardElement = document.createElement("div");
     
@@ -57,15 +73,65 @@ CardData.forEach((item, index) => {
     const CardText = document.createElement("div");
     const CardButton = document.createElement("button");
 
-    CardSection.append(CardElement);
+    
     CardElement.append(CardHeader, CardText, CardButton);
+    CardSection.append(CardElement);
 
 
-    CardElement.classList.add(`CardElement${index +1}`);
-    CardSection.setAttribute("class", "Cards");
-    CardHeader.classList.add(`CardHeader${index + 1}`);
-    CardText.classList.add(`CardText${index + 1}`);
-    CardButton.classList.add(`CardButton${index + 1}`);
+// 4 style classes with 4 'if' logic, +default style 
+
+
+// card style
+    if (item.cardstyle === "grey") {
+        CardElement.setAttribute("class", "CardElementGrey");
+    }
+      else if (item.cardstyle === "white") {
+        CardElement.setAttribute("class", "CardElementWhite");
+    } else if (item.cardstyle === "blue") {
+        CardElement.setAttribute("class", "CardElementBlue");
+    } else if (item.cardstyle === "image") {
+        CardElement.setAttribute("class", "CardElementImage");
+    } else {
+        CardElement.setAttribute("class", "CardElementGrey");
+    }
+
+
+// headerstyle
+    if (item.headerstyle === "blue") {
+        CardHeader.setAttribute("class", "CardHeaderBlue");
+    }
+      else if (item.headerstyle === "white") {
+        CardHeader.setAttribute("class", "CardHeaderWhite");
+    }  else {
+        CardHeader.setAttribute("class", "CardHeader1");
+    }
+
+
+// textstyle
+    if (item.textstyle === "blue") {
+        CardText.setAttribute("class", "CardTextBlue");
+    }
+      else if (item.textstyle === "grey") {
+        CardText.setAttribute("class", "CardTextGrey");
+    } else if (item.textstyle === "white") {
+        CardText.setAttribute("class", "CardTextWhite");
+    } else {
+        CardText.setAttribute("class", "CardTextBlue");
+    }
+
+
+// buttonstyle
+    if (item.buttonstyle === "white") {
+        CardButton.setAttribute("class", "CardButtonWhite");
+    }
+      else if (item.buttonstyle === "green") {
+        CardButton.setAttribute("class", "CardButtonGreen");
+    } else {
+        CardButton.setAttribute("class", "CardButtonWhite");
+    }
+
+
+    CardSection.setAttribute("class", "CardSection");
 
     CardHeader.append(item.title);
     CardText.append(item.text);
