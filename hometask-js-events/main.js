bbe79ef76444fb578980cd273b803d8a6300aad7
Yalale Lahunter97 <yahorlahunblr@icloud.com>
2025-04-21 22:05:52 +0300

    feat: implemented navigation menu and basic card structure

--- hometask-js-events/main.js
+++ hometask-js-events/main.js
@@ -1,12 +1,46 @@
-// For dropdown
 
+
+// menu bulma
+const DataMenu = [
+    "Jackets & Coats",
+    "Hoodies",
+    "T-shirts & Vests",
+    "Shirts",
+    "Blazers & Suits",
+    "Jeans",
+    "Trousers",
+    "Shorts",
+    "Underwear",
+    "Gift Sets",
+  ];
+  
+  const MenuValue = document.createDocumentFragment();
+  const MenuElements = document.createElement("ul");
+  MenuElements.setAttribute("class", "menu-list");
+
+
+  DataMenu.forEach((item) => {
+    const MenuElement = document.createElement("li");
+    const MenuElementInner = document.createElement("a");
+    MenuElementInner.textContent = item;
+    MenuElement.append(MenuElementInner);
+    MenuElements.append(MenuElement);
+  });
+  
+
+  MenuValue.append(MenuElements);
+  document.body.append(MenuValue);
+
+
+// dropdown
 const DataDropdown = {
     sort: "Sort by",
     drdbtn: "RECOMMENDED",
-    amount: "6 ITEMS",
+    amount: " ITEMS",
+    buttontext: "Add to bag",
 }; 
 
-// button bulma
+// select bulma
 const DropdownButton = document.createElement("option");
 const DropdownElement = document.createElement("select");
 const DropdownMain = document.createElement("div");
@@ -21,47 +55,42 @@ DropdownButton.append(DataDropdown.drdbtn);
 SortElement.setAttribute("class", "sort");
 DropdownMain.setAttribute("class", "dropdown");
 
-// header
-const HeaderElement = document.createElement("div");
-const AmountElement = document.createElement("div");
 
-AmountElement.append(DataDropdown.amount);
-AmountElement.setAttribute("class", "amountitems") // добавить счётчик на число
+// Cards
 
-HeaderElement.setAttribute("class", "header")
-HeaderElement.append(AmountElement, DropdownMain);
-
-
-const Header = document.getElementsByTagName("body") [0];
-Header.prepend(HeaderElement);
-
-
-
-
-/*
-const HeaderData = {
-    HeaderKey: "Last works",
-    ButtonHeader: "Explore Showcase",
-    ButtonCard: "Explore",
-
-// For header
-
-const HeaderElement = document.createElement("header");
-const H2Element = document.createElement("div");
-const HeaderButton = document.createElement("button");
-
-HeaderElement.append(H2Element, HeaderButton);
-HeaderElement.setAttribute("class", "Header");
-HeaderButton.setAttribute("class", "HeaderButton")
-
-H2Element.append(HeaderData.HeaderKey);
-HeaderButton.append(HeaderData.ButtonHeader);
-
-const Header = document.getElementsByTagName("body") [0];
-Header.prepend(HeaderElement);
-
-
-//For cards
+const CardData = [
+    {
+        name: "slub jersey t-shirt",
+        price: "$ 12.99",
+        photo: "slub-jersey-t-shirt-white",
+    },
+    {
+        name: "printed t-shirt",
+        price: "$ 12.99",
+        photo: "printed-t-shirt",
+    },
+    {
+        name: "cotton t-shirt",
+        price: "$ 12.99",
+        photo: "cotton-t-shirt",
+    },
+    {
+        name: "t-shirt with a motif",
+        price: "$ 12.99",
+        photo: "t-shirt-with-a-motif",
+    },
+    {
+        name: "cotton t-shirt regular fit",
+        price: "$ 12.99",
+        photo: "cotton-t-shirt-regular-fit",
+    },
+    {
+        name: "slub jersey t-shirt",
+        price: "$ 12.99",
+        photo: "slub-jersey-t-shirt-th",
+    },
+    {},
+];
 
 const CardValue = document.createDocumentFragment();
 const CardSection = document.createElement("section");
@@ -70,57 +99,74 @@ CardData.forEach((item) => {
     
     const CardElement = document.createElement("div");
     
-    const CardHeader = document.createElement("div");
-    const CardText = document.createElement("div");
+    const CardPhoto = document.createElement("div");
+    const CardName = document.createElement("div");
+    const CardPrice = document.createElement("div");
     const CardButton = document.createElement("button");
 
     
-    CardElement.append(CardHeader, CardText, CardButton);
+    CardElement.append(CardPhoto, CardName, CardPrice, CardButton);
     CardSection.append(CardElement);
 
 
-// classes from style name + default style 
-// card style
-CardElement.classList.add(`CardElement${item.cardstyle}`);
-if (!item.cardstyle) {
-    CardElement.setAttribute("class", "CardElementGrey");
+// class photo+name, default photo
+
+CardElement.classList.add("card-element");
+
+// background photo
+CardPhoto.classList.add(`photo-${item.photo}`);
+if (!item.photo) {
+    CardPhoto.setAttribute("class", "photo-no");
   }
-
-
-// headerstyle
-CardHeader.classList.add(`CardHeader${item.headerstyle}`);
-if (!item.headerstyle) {
-    CardHeader.setAttribute("class", "CardHeaderBlue");
-  }
-
 
 // textstyle
-CardText.classList.add(`CardText${item.textstyle}`);
-if (!item.textstyle) {
-    CardText.setAttribute("class", "CardTextBlue");
-  }
+CardName.classList.add("card-text");
 
+// pricestyle
+CardPrice.classList.add("card-price");
 
 // buttonstyle
-CardButton.classList.add(`CardButton${item.buttonstyle}`);
-if (!item.buttonstyle) {
-    CardButton.setAttribute("class", "CardButtonWhite");
-  }
+CardButton.classList.add("card-button");
 
 
+    CardSection.setAttribute("class", "card-section");
 
-    CardSection.setAttribute("class", "CardSection");
-
-    CardHeader.append(item.title);
-    CardText.append(item.text);
-    CardButton.append(HeaderData.ButtonCard);
+    CardName.append(item.name);
+    CardPrice.append(item.price);
+    CardButton.append(DataDropdown.buttontext);
 
     CardValue.append(CardSection);
-  
 
 });
 
+// select header
+const HeaderElement = document.createElement("div");
+const AmountElement = document.createElement("div");
 
-const Card = document.getElementsByTagName("body") [0];
-Card.append(CardValue);
-*/
+
+let CardCounter = CardData.length;
+AmountElement.append(`${CardCounter}${DataDropdown.amount}`);
+
+
+AmountElement.setAttribute("class", "amountitems")
+
+HeaderElement.setAttribute("class", "header")
+HeaderElement.append(AmountElement, DropdownMain);
+
+
+
+// page
+const PageElement = document.createElement("div");
+const PageCards = document.createElement("div");
+const PageMenu = document.createElement("div");
+
+PageElement.setAttribute("class", "container");
+PageMenu.setAttribute("class", "menu");
+PageCards.setAttribute("class", "cards");
+
+PageMenu.append(MenuElements);
+PageCards.append(HeaderElement, CardValue);
+PageElement.append(PageMenu, PageCards);
+
+const Page = document.getElementsByTagName("body") [0];
+Page.prepend(PageElement);
